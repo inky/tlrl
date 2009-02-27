d7b5c158dc74b0d0008d40741b73e42bda899e61
Liam Cooke <liamcooke@gmail.com>
2009-02-27 17:15:45 +0000

    Almost fool-proof(ish) (maybe)

--- tldr.py
+++ tldr.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 from __future__ import with_statement
-import httplib, urllib, ConfigParser, os, re
+import httplib, urllib, ConfigParser, optparse, os, re, sys
 
 host, api = 'www.instapaper.com', '/api'
 cfgfile = os.path.expanduser('~/.tldr')
 cfgsect = 'Instapaper'
 valid_url = re.compile('^https?://.+$').match
 status = {
-    200: 'Username and password are correct!',
-    201: 'URL added!',
-    403: 'Invalid username or password.',
-    500: 'The service encountered an error. Please try again later.',
+    200: ('Username and password are correct!', 1),
+    201: ('URL added!', 1),
+    403: ('Invalid username or password.', 0),
+    500: ('The service encountered an error. Please try again later.', 0),
 }
 
 def read_cfg():
@@ -28,25 +28,48 @@ def read_cfg():
         print "Empty configuration file created at", cfgfile
 
 def tldr(credentials, url=None):
+    # TODO check for blank username/password
     if url and not valid_url(url):
-        os.sys.write('URL is invalid.\n')
+        sys.stderr.write('URL is invalid.\n')
+        return
     params = credentials.items()
     if not params:
         return
     conn = httplib.HTTPSConnection(host)
-    api_uri = '/'.join((api, url and 'api' or 'authenticate'))
+    api_uri = '/'.join((api, url and 'add' or 'authenticate'))
     if url:
         params.extend([('url', url), ('auto-title', 1)])
     conn.request('GET', '?'.join((api_uri, urllib.urlencode(params))))
     return conn.getresponse().status
 
+def print_result(code):
+    if not code or code not in status:
+        return
+    if status[code][1]:
+        print status[code][0]
+    else:
+        sys.stderr.write(status[code][0] + '\n')
+
 def main():
     cred = read_cfg()
     if not cred:
         return
-    s = tldr(cred)
-    if s in status:
-        print status[s]
+
+    op = optparse.OptionParser(usage='usage: %prog URL')
+    op.add_option('-a', '--auth', action='store_true', default=False,
+                  help='verify your username and password')
+    op.set_description('Add URL to Instapaper. The first time this program is run, '\
+                       'a new config file is saved in your home folder, with blank '\
+                       'entries for your username and password.')
+    opts, args = op.parse_args()
+    if opts.auth:
+        print_result(tldr(cred))
+    elif not args:
+        op.print_help()
+    elif len(args) > 1:
+        sys.stderr.write('One URL at a time, please!\n')
+    else:
+        print_result(tldr(cred, args[0]))
 
 if __name__ == '__main__':
     main()
