45a1ddc4a89880d9e464bff850bc68b25409e1e8
Liam Cooke <liamcooke@gmail.com>
2009-02-27 16:48:19 +0000

    Status messages + rewriting

--- tldr.py
+++ tldr.py
@@ -3,15 +3,22 @@ from __future__ import with_statement
 import httplib, urllib, ConfigParser, os
 
 host, api = 'www.instapaper.com', '/api'
-cfgfile=os.path.expanduser('~/.tldr')
-cfgsect='Instapaper'
+cfgfile = os.path.expanduser('~/.tldr')
+cfgsect = 'Instapaper'
 
-def cred():
+status = {
+    200: 'Username and password are correct!',
+    201: 'URL added!',
+    403: 'Invalid username or password',
+    500: 'The service encountered an error. Please try again later.',
+}
+
+def read_cfg():
     cfg = ConfigParser.RawConfigParser()
     if os.path.isfile(cfgfile):
         cfg.read(cfgfile)
         u, p = cfg.get(cfgsect, 'username'), cfg.get(cfgsect, 'password')
-        return [('username', u), ('password', p)]
+        return dict(username=u, password=p)
     else:
         cfg.add_section(cfgsect)
         cfg.set(cfgsect, 'username', '')
@@ -20,8 +27,8 @@ def cred():
             cfg.write(f)
         print "Empty configuration file created at", cfgfile
 
-def tldr(url=None):
-    params = cred()
+def tldr(credentials, url=None):
+    params = credentials.items()
     if not params:
         return
     conn = httplib.HTTPSConnection(host)
@@ -30,8 +37,17 @@ def tldr(url=None):
     if url:
         params.extend([('url', url), ('auto-title', 1)])
     conn.request('GET', '?'.join((api_uri, urllib.urlencode(params))))
-    resp = conn.getresponse()
-    print resp.status, resp.reason
+    return conn.getresponse().status
+
+def main()
+    cred = read_cfg()
+    if cred:
+        s = tldr(cred)
+        if s in status:
+            print status[s]
+        elif s:
+            print "Bad request. This shouldn't happen -- "\
+                  "the program may be out of date."
 
 if __name__ == '__main__':
-    tldr()
+    main()
