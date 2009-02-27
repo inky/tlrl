84479ca6f78302347a9fedede5641ab27f9ff9b7
Liam Cooke <liamcooke@gmail.com>
2009-02-27 16:54:17 +0000

    URL checking + cleanup

--- tldr.py
+++ tldr.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 from __future__ import with_statement
-import httplib, urllib, ConfigParser, os
+import httplib, urllib, ConfigParser, os, re
 
 host, api = 'www.instapaper.com', '/api'
 cfgfile = os.path.expanduser('~/.tldr')
 cfgsect = 'Instapaper'
-
+valid_url = re.compile('^https?://.+$').match
 status = {
     200: 'Username and password are correct!',
     201: 'URL added!',
-    403: 'Invalid username or password',
+    403: 'Invalid username or password.',
     500: 'The service encountered an error. Please try again later.',
 }
 
@@ -28,26 +28,25 @@ def read_cfg():
         print "Empty configuration file created at", cfgfile
 
 def tldr(credentials, url=None):
+    if url and not valid_url(url):
+        os.sys.write('URL is invalid.\n')
     params = credentials.items()
     if not params:
         return
     conn = httplib.HTTPSConnection(host)
-    conn.set_debuglevel(1)
     api_uri = '/'.join((api, url and 'api' or 'authenticate'))
     if url:
         params.extend([('url', url), ('auto-title', 1)])
     conn.request('GET', '?'.join((api_uri, urllib.urlencode(params))))
     return conn.getresponse().status
 
-def main()
+def main():
     cred = read_cfg()
-    if cred:
-        s = tldr(cred)
-        if s in status:
-            print status[s]
-        elif s:
-            print "Bad request. This shouldn't happen -- "\
-                  "the program may be out of date."
+    if not cred:
+        return
+    s = tldr(cred)
+    if s in status:
+        print status[s]
 
 if __name__ == '__main__':
     main()
